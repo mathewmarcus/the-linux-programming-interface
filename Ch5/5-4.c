4e59160412d0704d162524a307d6ffbecb7bb0ac
mathewmarcus <mathewmarcus456@gmail.com>
2017-10-24 17:32:34 -0500

    finished 5-4

---
+++ Ch5/5-4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int my_dup(int old_fd) {
+  return fcntl(old_fd, F_DUPFD, 0);
+}
+
+int my_dup2(int old_fd, int new_fd) {
+  if (old_fd == new_fd) { /* old file descriptor is the same as new file descriptor, return this value */
+    fprintf(stderr, "File descriptors are identical\n");
+    return new_fd;
+  }
+  else if (fcntl(new_fd, F_GETFD) != -1) { /* new file descriptor already exists, close it */
+    if (close(new_fd) == -1) {
+      perror("Failed to close already open new file descriptor");
+      return -1;
+    }
+  }
+
+  return fcntl(old_fd, F_DUPFD, new_fd);
+    
+}
