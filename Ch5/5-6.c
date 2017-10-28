f77abf41c3098d0813549ebd4521da50c81a165f
mathewmarcus <mathewmarcus456@gmail.com>
2017-10-28 14:00:28 -0500

    finished 5-6.c

---
+++ Ch5/5-6.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/* Output: Giddayworld */
+int main(int argc, char *argv[argc])
+{
+  char *file = "foo.txt";
+  int fd1, fd2, fd3;
+
+  fd1 = open(file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  fd2 = dup(fd1);
+  fd3 = open(file, O_RDWR);
+  write(fd1, "Hello,", 6);
+  write(fd2, "world", 6);
+  lseek(fd2, 0, SEEK_SET);
+  write(fd1, "HELLO,", 6);
+  write(fd3, "Gidday", 6);
+  return 0;
+}
