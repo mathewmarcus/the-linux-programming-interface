247684d1c8f874023aea21e9b0e0b2131dd09de5
mathewmarcus <mathewmarcus456@gmail.com>
2017-10-28 14:00:36 -0500

    finished 5.7.c

---
+++ Ch5/5-7.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+ssize_t my_writev(int fd , const struct iovec *iov , int iovcnt) {
+  char *buffer;
+  int len, j;
+  ssize_t bytes;
+
+  for (int i = len = 0; i < iovcnt; ++i)
+    len += iov[i].iov_len;
+
+  buffer = malloc(sizeof(char) * len);
+  for (int i = j = 0; i < iovcnt; ++i)
+    for (size_t k = 0; k < iov[i].iov_len; ++k)
+      buffer[j++] = ((char*) iov[i].iov_base)[k];
+
+  if ((bytes = write(fd, buffer, len)) == -1) {
+    perror("Failed to write bytes");
+    free(buffer);
+    return errno;
+  }
+
+  free(buffer);
+  return bytes;
+}
+
+ssize_t my_readv(int fd , const struct iovec *iov , int iovcnt) {
+  char *buffer;
+  int len, j;
+  ssize_t bytes;
+
+  for (int i = len = 0; i < iovcnt; ++i)
+    len += iov[i].iov_len;
+
+  buffer = malloc(sizeof(char) * len);
+  if ((bytes = read(fd, buffer, len)) == -1) {
+    perror("Failed to read bytes");
+    free(buffer);
+    return errno;
+  }
+
+  for (int i = j = 0; i < iovcnt; ++i)
+    for (size_t k = 0; k < iov[i].iov_len; ++k)
+      ((char*) iov[i].iov_base)[k] = buffer[j++];
+
+  free(buffer);
+  return bytes;
+}
